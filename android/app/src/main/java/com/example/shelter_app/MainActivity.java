6b248a8af26da472bae28db7e25a12cea4b413f5
Kamski16 <kamskii196@gmail.com>
2021-11-20 16:14:07 +0100

    Navigation

--- android/app/src/main/java/com/example/shelter_app/MainActivity.java
+++ android/app/src/main/java/com/example/shelter_app/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.shelter_app;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
+        bottomNavigationView.setSelectedItemId(R.id.calendar);
     }
+
+    FragmentCalendar calendarFragment = new FragmentCalendar();
+    FragmentMain mainFragment = new FragmentMain();
+    FragmentProfile profileFragment = new FragmentProfile();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment).commit();
+                return true;
+
+            case R.id.main:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
+                return true;
+
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
+
 }
